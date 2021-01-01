e50f109d80ffccba383e72290eade1c66790bcc0
Neyelson alves <neyelson@gmail.com>
2021-01-01 05:23:14 -0300

    Sistema de tabbar e views implementadas.

--- App.js
+++ App.js
@@ -1,26 +1,35 @@
 import * as React from 'react';
 import { View, Image, Text, StyleSheet, Dimensions, SafeAreaView, Platform, StatusBar } from 'react-native';
 import { TabView, TabBar, SceneMap } from 'react-native-tab-view';
-import home_icon from './assets/tabicons/numeric-1.png';
 
 const FirstRoute = () => (
-  <View style={[styles.scene, { backgroundColor: 'white' }]} />
+  <View style={styles.viewprincipal} >
+  <Text > CONTEÚDO DA PRIMEIRA ABA </Text>
+  </View>
 );
 
 const SecondRoute = () => (
-  <View style={[styles.scene, { backgroundColor: '#white' }]} />
+  <View style={styles.viewprincipal} >
+  <Text > CONTEÚDO DA SEGUNDA ABA </Text>
+  </View>
 );
 
 const TerceiraRota = () => (
-  <View style={[styles.scene, { backgroundColor: '#white' }]} />
+  <View style={styles.viewprincipal} >
+  <Text > CONTEÚDO DA TERCEIRA ABA </Text>
+  </View>
 );
 
 const QuartaRota = () => (
-  <View style={[styles.scene, { backgroundColor: '#white' }]} />
+  <View style={styles.viewprincipal} >
+  <Text > CONTEÚDO DA QUARTA ABA </Text>
+  </View>
 );
 
 const QuintaRota = () => (
-  <View style={[styles.scene, { backgroundColor: '#white' }]} />
+  <View style={styles.viewprincipal} >
+  <Text > CONTEÚDO DA QUINTA ABA </Text>
+  </View>
 );
 
 const initialLayout = { width: Dimensions.get('window').width };
@@ -45,32 +54,32 @@ export default function TabViewExample() {
   });
 
   const renderTabBar = props => (
-    <TabBar
+    <TabBar 
       {...props}
       indicatorStyle={{ backgroundColor: 'black' }}
-      style={{ backgroundColor: 'white' }}
-      labelStyle={{ color: 'black' }}
+      style={{ backgroundColor: 'white',  }}
       scrollEnabled={true}
-      renderIcon={({ route }) => ( <Image style={styles.topbaricon} source={route.icon} /> )}
-      renderLabel={({ route }) => ( <Text style={styles.topbartext}> {route.title} </Text> )}
+      // poderia renderizar o icone separado só abaixo
+     // renderIcon={({ route }) => (  )}
+      renderLabel={({ route }) => ( 
+       <Text style={styles.topbartext}> <Image style={styles.topbaricon} source={route.icon} /> {route.title} </Text>
+      )}
           />
   );
   
   return (
     <SafeAreaView style={styles.container}>
     <TabView
-    
      renderTabBar={renderTabBar}
       navigationState={{ index, routes }}
       renderScene={renderScene}
       onIndexChange={setIndex}
       initialLayout={initialLayout}
       //prop de baixo é importante pois otimiza a velocidade do app
-      lazy={true}
+      //lazy={true}
       //swipeEnabled={false}
     />
 </SafeAreaView>
-
   );
   
 }
@@ -79,21 +88,28 @@ const styles = StyleSheet.create({
   scene: {
     flex: 1,
   },
-  container: {
+    container: {
     flex: 1,
     backgroundColor: "white",
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
   },
-  topbaricon: {
-    backgroundColor: "white",
-    width: 30,
-    height: 30,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-  },
   topbartext: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
+    backgroundColor: 'white'
+  },
+  topbaricon: {
+    width:30,
+    height:30,
+  },
+
+  viewprincipal: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
   },
 
 });
